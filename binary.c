80e09912619c155eb86512f9f01317ad1e0336e6
Billyrogers61 <rogerskim61@gmail.com>
2021-10-03 07:27:33 -0700

    Binary

---
+++ binary.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * itob - Change to binary.
+ * @list: int to change.
+ * Return: string with binary.
+ */
+char *itob(va_list list)
+{
+	int j = 0, twos = 1;
+	int i, k;
+	char *s;
+
+	k = va_arg(list, int);
+	i = k;
+
+	/*malloc up to max int in binary */
+	s = malloc(sizeof(char) * 33);
+	if (s == NULL)
+		return (NULL);
+
+	/* account for negative numbers with '1' at index 0 */
+	if (k < 0)
+	{
+		s[0] = 1 + '0';
+		j++;
+		k *= -1;
+		i *= -1;
+	}
+
+	/* find biggest power of 2 */
+	while (k > 1)
+	{
+		k /= 2;
+		twos *= 2;
+	}
+	s[j] = '\0';
+	return (s);
+}
