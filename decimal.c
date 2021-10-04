4afad78a796de2e3d2f53c2f6291a12e4c83401e
Joy879 <joywanjiru879@gmail.com>
2021-10-04 06:34:41 -0700

    Adjust conversion specifier for decimal

--- decimal.c
+++ decimal.c
@@ -1,70 +1,32 @@
 #include "main.h"
-
 /**
- * _itos - Function to convert an int to a string.
- * @div: multiple of 10
- * @length: size of the number.
- * @n: number to convert to a string.
- * Return: string.
- **/
-char *_itos(int div, int length, int n)
+ * print_integer - Prints an integer
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_integer(va_list list)
 {
-	char *str;
-	int i = 0;
+	int num_length;
 
-	str = malloc(sizeof(char) * length + 2);
-	if (str == NULL)
-		return (NULL);
-	if (n < 0)
-	{
-		str[0] = '-';
-		i++;
-	}
-	while (n < 0) /* convert each num to string */
-	{
-		str[i] = ((n / div) * -1 + '0');
-		n = n % div;
-		div /= 10;
-		i++;
-	}
-	while (div >= 1) /* same, this case for positives */
-	{
-		str[i] = ((n / div) + '0');
-		n = n % div;
-		div /= 10;
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+	num_length = print_number(list);
+	return (num_length);
 }
 
 /**
- * print_d - gets length to put in _itos
- * @list: takes arg
- * Return: integar string
- **/
-char *print_d(va_list list)
+ * unsigned_integer - Prints Unsigned integers
+ * @list: List of all of the argumets
+ * Return: a count of the numbers
+ */
+int unsigned_integer(va_list list)
 {
-	int length, div, n, temp;
+	unsigned int num;
 
-	n = va_arg(list, int);
-	temp = n;
-	length = 0;
-	div = 1;
+	num = va_arg(list, unsigned int);
 
-	if (n == 0) /* account for 0 having length 1 */
-	{
-		length++;
-		return (_itos(div, length, n));
-	}
+	if (num == 0)
+		return (print_unsgined_number(num));
 
-	while (temp != 0) /* find multiple of ten to divide */
-	{
-		length += 1;
-		if (length > 1)
-			div *= 10;
-		temp /= 10;
-	}
-
-	return (_itos(div, length, n));
+	if (num < 1)
+		return (-1);
+	return (print_unsgined_number(num));
 }
