ac2a49c9eba77064481a7e51a31e12a7b618ccd2
Billyrogers61 <rogerskim61@gmail.com>
2021-10-04 07:22:25 -0700

    Update header file

--- main.h
+++ main.h
@@ -9,6 +9,9 @@
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 void write_base(char *str);
+char *rev_string(char *s);
+unsigned int base_len(unsigned int num, int base);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 /*Printf functions*/
 int _printf(const char *format, ...);
@@ -18,6 +21,10 @@ int _write_char(char c);
 int print_percent(__attribute__((unused))va_list list);
 int print_binary(va_list list);
 int print_integer(va_list);
+int print_number(va_list args);
+int parser(const char *format, conver_t f_list[], va_list arg_list);
+int print_unsgined_number(unsigned int n);
+
 
 
 
